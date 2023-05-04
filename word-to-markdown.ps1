d79d0af4403aaaadd860045dcff200335785dc87
joshknopp <joshknopp@users.noreply.github.com>
2023-05-04 13:34:30 -0400

    Update word-to-markdown.ps1

--- word-to-markdown.ps1
+++ word-to-markdown.ps1
@@ -6,30 +6,25 @@ $word = New-Object -ComObject Word.Application
 $doc = $word.Documents.Open($docPath, $false, $true)
 
 # Convert the document to HTML and remove the <head> and <body> tags
-$html = $doc.Content.EnhancedMarkup
-$html = $html.Replace("<head>", "").Replace("</head>", "").Replace("<body>", "").Replace("</body>", "")
+$html = $doc.Content.WordOpenXML
+$html = $html -replace "</?strong>", "**" # Replace <strong> tags with Markdown bold
+$html = $html -replace "</?em>", "*" # Replace <em> tags with Markdown italic
+$html = $html -replace "</?u>", "<u>" # Replace <u> tags with Markdown underline
+$html = $html -replace "</?strike>", "<del>" # Replace <strike> tags with Markdown strikethrough
+$html = $html -replace "<a.*?>|</a>", "" # Remove <a> tags
+$html = $html -replace "<br>", "`r`n" # Replace <br> tags with Windows line breaks
+$html = $html -replace "<p.*?>", "" # Remove <p> tags
+$html = $html -replace "</p>", "`r`n`r`n" # Replace </p> tags with two line breaks
 
 # Convert the HTML to Markdown
-$markdown = ""
-$regex = [regex]::new("<p.*?>(.*?)<\/p>")
-$matches = $regex.Matches($html)
-foreach ($match in $matches) {
-    $paragraph = $match.Groups[1].Value
-    $paragraph = $paragraph -replace "<(\/)*strong>", "**" # Replace <strong> tags with Markdown bold
-    $paragraph = $paragraph -replace "<(\/)*em>", "*" # Replace <em> tags with Markdown italic
-    $paragraph = $paragraph -replace "<(\/)*u>", "<u>" # Replace <u> tags with Markdown underline
-    $paragraph = $paragraph -replace "<(\/)*strike>", "<del>" # Replace <strike> tags with Markdown strikethrough
-    $paragraph = $paragraph -replace "<(\/)*a>", "" # Remove <a> tags
-    $paragraph = $paragraph -replace "`r`n", "`n" # Remove any Windows line breaks
-    $markdown += $paragraph + "`n`n"
-}
+$markdown = $html
 
 # Clean up the Markdown formatting
 $markdown = $markdown -replace "^(\s*)<li>", "`$1* " # Replace <li> tags with Markdown bullet points
-$markdown = $markdown -replace "<(\/)*table.*?>", "|_. " # Replace <table> tags with Markdown tables
-$markdown = $markdown -replace "<(\/)*tr.*?>", "|" # Replace <tr> tags with Markdown table rows
-$markdown = $markdown -replace "<(\/)*td.*?>", "|" # Replace <td> tags with Markdown table cells
-$markdown = $markdown -replace "`n`n`n+", "`n`n" # Remove excess line breaks
+$markdown = $markdown -replace "<table.*?>", "" # Remove <table> tags
+$markdown = $markdown -replace "<tr.*?>", "" # Remove <tr> tags
+$markdown = $markdown -replace "<td.*?>", "|" # Replace <td> tags with Markdown table cells
+$markdown = $markdown -replace "`r`n`r`n`r`n+", "`r`n`r`n" # Remove excess line breaks
 
 # Close the document and quit Word
 $doc.Close()
